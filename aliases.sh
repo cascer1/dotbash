23159850946bfa555faa6b68701a7510a26bbb74
Cas Eliëns <cas.eliens@isaac.nl>
2020-05-14 13:06:39 +0200

    Add usernames alias

--- aliases.sh
+++ aliases.sh
@@ -6,6 +6,8 @@ alias removegps='exiftool -gps:all= -xmp:geotag= -m'
 alias removetimestamp="sed -r 's/\[[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}\s[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\]//g' -i --"
 alias tohex='printf "%x\n"'
 alias hextime='tohex $(date +%s)'
+alias usernames='apg -a 0 -m 6 -x 6 -n 40 -M L'
+
 
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 
