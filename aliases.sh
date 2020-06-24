ed8f6fb1553fcf0b025bb803ad0f6e6414b552b5
Cas Eliens <cas@laptop.cas.eliens.co>
2020-06-24 18:44:01 +0200

    Add pdf compress command

--- aliases.sh
+++ aliases.sh
@@ -7,7 +7,7 @@ alias removetimestamp="sed -r 's/\[[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}\s[0-9]{1,2}:[0
 alias tohex='printf "%x\n"'
 alias hextime='tohex $(date +%s)'
 alias usernames='apg -a 0 -m 6 -x 6 -n 40 -M L'
-
+alias compresspdf='gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sOutputFile=compressed.pdf'
 
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 
